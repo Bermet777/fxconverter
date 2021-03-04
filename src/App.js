f70e3ff9c355dd9152e8eae140d0368b0294b0a7
Bermet777 <bermet.alibek@gmail.com>
2021-03-04 12:22:43 +0100

    currency app

--- src/App.js
+++ src/App.js
@@ -1,47 +1,61 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import CurrenciesSums from './components/CurrenciesSums';
 
+const URL= 'https://api.exchangeratesapi.io/latest?base=USD';
+
+const URLe='https://api.exchangeratesapi.io/latest?base=USD&symbols=';
+
 function App() {
 
-  const [formState, setFormState] = useState({
-    amount: 0,
-    currency: "KGS"
-  });
+ 
+    const [convertedCurrencies, setConvertedCurrencies] = useState([]);
+    const [amount, setAmount] = useState();
+    const [selectedCurrency,setSelectedCurrency]=useState();
+    const [currencyOptions,setCurrencyOptions]= useState([])
 
-  const [convertedCurrencies, setConvertedCurrencies] = useState([]);
+//fetching data from API and storing currency rates
+  useEffect(() => {    
+  fetch(URL)         
+   .then(request => request.json ()) 
+   .then (data => setCurrencyOptions ([...Object.keys(data.rates)]));
 
-  const currenciesRatesInUSD = {
-    "KGS": 84.54,
-    "EUR": 0.83,
-    "RUB": 73.93
-  }
-
+  }, []);
+// fetching rate for selected currency
+  useEffect(() => { 
+    if (selectedCurrency!=null)    {
+      fetch(URLe+selectedCurrency)         
+      .then(request => request.json ()) 
+      .then (data => (data));
+    }
+    
+  
+    }, [selectedCurrency]);
+  
   const onInputChange = (e) => {
     
-    setFormState({
-      ...formState,
-      [e.target.name]: e.target.value
-    })
+    setAmount(e.target.value) }
     
+    
+  const onSelectChange = (e) => {
+    setSelectedCurrency(e.target.value)
   }
 
   const onSubmit = (e) => {
 
     e.preventDefault();
-    setConvertedCurrencies([])
+    //setConvertedCurrencies([])
 
-    const {currency, amount} = formState;
+   // const {currency, amount} = formState;
 
-    setConvertedCurrencies([{
-      currency: currency,
-      amount: +amount,
-      amountInUSD: (+amount/currenciesRatesInUSD[currency]).toFixed(3)
-    }])
+    //setConvertedCurrencies([{
+    //  currency: currency,
+    //  amount: +amount,
+    //  amountInUSD: (+amount/currenciesRatesInUSD[currency]).toFixed(3)
+    //}])
 
   }
 
-  console.log(convertedCurrencies);
 
   return (
     <div className="App">
@@ -51,25 +65,25 @@ function App() {
         <h1>
           Calculate payment in USD
         </h1>
+        
 
         <input 
           name="amount" 
           type="number"
           placeholder="Amount" 
           onChange={onInputChange} 
-          value={formState.amount}
+          value={amount}
           required
           />
 
         <select 
           name="currency" 
-          onChange={onInputChange}
-          value={formState.currency}
+          onChange={onSelectChange}
+          value={selectedCurrency}
           required
-          >
-          <option value="KGS">{'Som (KGS)'}</option>
-          <option value="EUR">{'Euro (EUR)'}</option>
-          <option value="RUB">{'Ruble (RUB)'}</option>
+          > 
+          {currencyOptions.map(element => (<option key={element} > {element} </option>))}
+         
         </select>
 
          <button className="button">
